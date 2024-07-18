f50a71631884e5a652b05c2fa7c2a81d2b83fef6
JoshuaPermejo <permejojoshua57@gmail.com>
2024-07-18 17:35:54 +0800

    fix all and added another feature

---
+++ JS/dashboard.js
@@ -0,0 +1,30 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    reponsive: true
+  }
+});
+
+const showMenu = (headerToggle, navbarId) =>{
+  const toggleBtn = document.getElementById(headerToggle),
+  nav = document.getElementById(navbarId)
+  
+  // Validate that variables exist
+  if(headerToggle && navbarId){
+      toggleBtn.addEventListener('click', ()=>{
+          // We add the show-menu class to the div tag with the nav__menu class
+          nav.classList.toggle('show-menu')
+      })
+  }
+}
+showMenu('header-toggle','sideBar')
